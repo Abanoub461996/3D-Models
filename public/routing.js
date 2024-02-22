57596effd2e6695ee5969b13707421126338e3db
Abanoub461996 <pophtc.aa@gmail.com>
2024-02-22 21:37:21 +0200

    fix: fixing the vite config for github delpoyment

--- public/routing.js
+++ public/routing.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".routing_btn").forEach((btn) => {
   btn.addEventListener("click", () => {
     const path = btn.dataset.model;
-    window.location.href = path === '/' ? '/3D-Model' : path;
+    window.location.href = path === '/' ? '/3D-Model/' : path;
   });
 });
